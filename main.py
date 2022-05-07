26740d526c8940aa55777cd133e60cad9ea95b89
FrozZzy1 <frozzzy71@gmail.com>
2022-05-07 17:51:30 +0700

    refinement

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def search_name():
         for file in files:
             filename = os.path.join(root, file)
 
-            if filename.endswith('.xml'):
+            if filename.endswith('.xml') and filename != "workspace.xml":
                 sd = win32security.GetFileSecurity(filename, win32security.OWNER_SECURITY_INFORMATION)
                 owner_sid = sd.GetSecurityDescriptorOwner()
                 name, domain, type = win32security.LookupAccountSid(None, owner_sid)
@@ -19,35 +19,38 @@ def search_name():
 
     return name
 
-owner_name = search_name()
-filenames = []
 
-def search_files(owner, list):
+def search_files(owner):
     folder = os.getcwd()
-
+    lst = []
+    extensions = ['.doc', '.docx', '.ppt', '.pptx', '.xls', '.xlsx']
     for root, dirs, files in os.walk(folder):
         for file in files:
             filename = os.path.join(root, file)
-            if (filename.endswith('doc') or filename.endswith('docx') or
-            filename.endswith('ppt') or filename.endswith('pptx') or
-            filename.endswith('xls') or filename.endswith('xlsx')):
+            ext = os.path.splitext(filename)[1]
+            if ext in extensions:
                 sd = win32security.GetFileSecurity(filename, win32security.OWNER_SECURITY_INFORMATION)
                 owner_sid = sd.GetSecurityDescriptorOwner()
                 name, domain, type = win32security.LookupAccountSid(None, owner_sid)
 
                 if name == owner:
-                    list.append(os.path.join(file))
+                    lst.append(os.path.join(file))
 
-    return list
+    return lst
 
-filenames = search_files(owner_name, filenames)
 
-def filing():
+def filing(file_names):
     file = open('filenames.txt', 'w')
 
-    for i in filenames:
+    for i in file_names:
         file.write(i)
         file.write("\n")
     file.close()
 
-filing()
+def main():
+    owner_name = search_name()
+    filenames = search_files(owner_name)
+    filing(filenames)
+
+if __name__ == '__main__':
+    main()
